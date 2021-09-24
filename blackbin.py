cafdcd7ae822f50d69b489b46c6f7f2047df469b
loglux <call2vlad@outlook.com>
2021-09-24 16:20:59 +0100

    Initial Commit

---
+++ blackbin.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
+import datetime as dt
+from O365 import Account
